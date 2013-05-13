3cfa9a5d2ddde4b4ef7d288fc30c2b4202960386
metime00 <metime00@gmail.com>
2013-05-13 16:23:42 -0400

    Updated Singer to use subdivisons

--- SongSingums/Singer.fs
+++ SongSingums/Singer.fs
@@ -3,35 +3,53 @@ open Util
 
 /// ratio of note to base note represented as a float
 type Ratio = float
-/// length of a note in subdivisions by fraction of the parent node's time
-type Length = float
+/// location of a note's subdivision by fraction of the parent node's time
+type Loc = float
 
 /// node in a tree of notes or beats
-type NoteNode (parent : NoteNode) =
-    let mutable ratio : Ratio = 0.0
-    let mutable length : Length = 0.0
+type NoteNode (r : Ratio, l : Loc) =
+    let mutable ratio : Ratio = r
+    let mutable loc : Loc = l
     let mutable children : NoteNode list = List.empty
 
     member this.Ratio = 
         match children with
-        | some -> 
-        | List.empty -> None
-    member this.Length = length
+        | some -> Some ratio
+        | children when List.isEmpty children -> None
+
+    member this.Location = loc
 
     /// returns a list of the notes children
     member this.Children = children
 
     member this.AddChildren (input : NoteNode list) =
-        ()
+        children <- input
 
     member this.RemoveChildren () =
         children <- List.empty
 
 type Singer () =
-    /// time signature, expressed as subdivisions of notes
-    let mutable time = null
+    let timeGen () =
+        /// creates a list of the locations of beats
+        let gen left right level =
+            [
+                /// true middle
+                let mid = left + right / 2.0
+                /// displaced middle
+                let disp = (float (r.Next (int (left * H * prec), int (right * H * prec))) / prec)
+                // booleans to check if the left or right midpoints will go
+                let lB = r.NextDouble () < H
+                let rB = r.NextDouble () < H
+
+            ]
+        gen 0.0 1.0 0
+
+    /// time signature, expressed as a list of note subdivisions
+    let mutable time : NoteNode list = List.empty
     /// motifs are song snippets expressed as arrays of notes
-    let mutable motifs = null
+    let mutable motifs : NoteNode list = List.empty
+    let melody = NoteNode (1.0, 1.0)
+    let rhythm = NoteNode (0.5, 1.0)
 
     member this.NextMeasure () =
         ()
--- SongSingums/Util.fs
+++ SongSingums/Util.fs
@@ -6,6 +6,10 @@ let n = 44100
 let multiplier = 0.07
 /// random number generator
 let r = new System.Random ()
+/// Singer level of precision
+let prec = 10.0
+/// Erraticity, lower is less erratic
+let H = 0.5
 
 type Sound =
     /// frequency of note
