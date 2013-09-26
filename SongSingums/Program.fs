d19a3c5b09a680c5e88065d0acff117381d30d01
John <metime00@gmail.com>
2013-09-26 15:15:51 -0400

    Made BasicSinger a validly inheritor of Singer. Although Audio still hasn't been changed to support the new structure of code

--- SongSingums/Program.fs
+++ SongSingums/Program.fs
@@ -1,5 +1,4 @@
-﻿//PRE: make Singer inheritable or an abstract class so more generators can be made using the same basic methods for use in outside parts of the program. minimize the interaction of singer with the outside world. Make Singer use Waves so Audio doesn't have to care what instrument or voices that singer has, and just interprets the 
-//1. make simple generator that generates a minute long song; single wave, single voice melody from a time signature and base frequency, that can be added to for different overtone, waveforms, and voice additions. Make it modular
+﻿//1. make simple generator that generates a minute long song; single wave, single voice melody from a time signature and base frequency, that can be added to for different overtone, waveforms, and voice additions. Make it modular
 //2. make program recognize rests and use them in generation
 //3. Make generator that generates a melody and bass voice, uses overtones and different waves, and has song progression over the course of the song
 //4. Audio's main purpose will be continuous streaming
--- SongSingums/Singers.fs
+++ SongSingums/Singers.fs
@@ -6,14 +6,14 @@ open Waves
 type BasicSinger (tem, err) =
     inherit Singer(tem, err)
 
-    ///creates a ratio from a maximum number that can be used either in the numerator or denominator and a maximum total value of the fraction
-    let ratioGen maxRatio (maxQuo : Fraction) =
-        let num = r.Next (1, maxRatio + 1)
-        let den = r.Next (max 1 ((int) (ceil ((num / maxQuo).ToFloat ()))), min (maxRatio + 1) ((int) ((num * maxQuo).ToFloat ())))
-        Ratio (num, den)
+    /// motifs are song snippets expressed as arrays of notes
+    let mutable motifs : NoteNode list = List.empty
+
+    /// the main voice of the song
+    let melody = NoteNode (Fraction (1), Fraction (0))
         
     /// generates a time signature
-    let timeGen () =
+    override this.TimeGen () =
         /// creates a list of the locations of beats
         let rec gen (left : Loc) (right : Loc) level =
             [
@@ -30,36 +30,31 @@ type BasicSinger (tem, err) =
                 let newMid = mid + left + disp
                 // booleans to check if the left or right midpoints will go, cutoff should be 4.0
                 if level < 4.0 then
-                    if r.NextDouble () < (H.ToFloat ()) ** (level / 2.0) && right - left > interval then yield! gen left newMid (level + 1.0)
-                    if r.NextDouble () < (H.ToFloat ()) ** (level / 2.0) then yield newMid
-                    if r.NextDouble () < (H.ToFloat ()) ** (level / 2.0) && right - left > interval then yield! gen newMid right (level + 1.0)
-//                    elif r.NextDouble () < (H.ToFloat ()) ** (level / 2.0) && right - left > interval then //three evenly spaced triplet notes
+                    if r.NextDouble () < (this.H.ToFloat ()) ** (level / 2.0) && right - left > interval then yield! gen left newMid (level + 1.0)
+                    if r.NextDouble () < (this.H.ToFloat ()) ** (level / 2.0) then yield newMid
+                    if r.NextDouble () < (this.H.ToFloat ()) ** (level / 2.0) && right - left > interval then yield! gen newMid right (level + 1.0)
+//                    elif r.NextDouble () < (this.H.ToFloat ()) ** (level / 2.0) && right - left > interval then //three evenly spaced triplet notes
 //                        let shiftR = right - left
 //                        for i = 1 to 2 do
 //                            yield (i * shiftR / 3) + left
-//                    elif r.NextDouble () < (H.ToFloat ()) ** (level / 2.0) then yield newMid //triplets creates a situation where the time signature can become very empty
+//                    elif r.NextDouble () < (this.H.ToFloat ()) ** (level / 2.0) then yield newMid //triplets creates a situation where the time signature can become very empty
                         
             ]
         Loc 0 :: gen (Loc 0) (Loc 1) 1.0
 
-    /// time signature, expressed as a list of note subdivisions
-    let timeSig : Fraction list = timeGen ()
-    /// motifs are song snippets expressed as arrays of notes
-    let mutable motifs : NoteNode list = List.empty
-
     /// returns the initial children for a NoteNode based on the time signature, and their frequency ratios
-    let nodeInit time =
+    override this.NodeInit time =
         ///largest number allowed in the numerator or denominator, should be 6
         let maxRatio = 6
         ///how many times greater a part of the ratio can be than the other part
         let maxQuo = Fraction (2, 1)
         [|
             for i in time do
-                yield NoteNode (ratioGen maxRatio maxQuo, i)
+                yield NoteNode (this.RatioGen maxRatio maxQuo, i)
         |]
 
     /// generates a subdivision of a melody, returning two or three NoteNodes
-    let melGen () =
+    override this.MelGen () =
         /// largest number allowed in the numerator or denominator
         let maxRatio = 6
         /// how many times greater a part of the ratio can be than the other part
@@ -70,7 +65,7 @@ type BasicSinger (tem, err) =
         if triple then //three equal spaced children notes
             [
                 for i = 0 to 2 do
-                    yield NoteNode (ratioGen maxRatio maxQuo, Fraction (i, 3))
+                    yield NoteNode (this.RatioGen maxRatio maxQuo, Fraction (i, 3))
             ]
         else //two unequally spaced notes
             /// numerator can only be up to one less this number, with denominator only up to two more
@@ -79,24 +74,22 @@ type BasicSinger (tem, err) =
                 let range = 6
                 let numL = r.Next (1, range) //range is one less so the note location is never 1.0
                 let denL = r.Next (int numL + 1, range + 2)
-                yield NoteNode (ratioGen maxRatio maxQuo, Fraction (0))
-                yield NoteNode (ratioGen maxRatio maxQuo, Fraction (numL, denL))
+                yield NoteNode (this.RatioGen maxRatio maxQuo, Fraction (0))
+                yield NoteNode (this.RatioGen maxRatio maxQuo, Fraction (numL, denL))
             ]
-    //the main voice of the song
-    let melody = NoteNode (Fraction (1), Fraction (0))
 
-    let nextMeasure () =
+    override this.NextMeasure () =
         melody.RemoveChildren ()
-        melody.AddChildren (nodeInit timeSig)
+        melody.AddChildren (this.NodeInit this.TimeSig)
 
         [|
             yield melody
         |]
 
     /// Converts an array of amplitudes and locations to a buffer
-    let bufferMake (input : (Ratio * Loc)[]) (init : float) =
+    override this.BufferMake (input : (Ratio * Loc)[]) (init : float) =
         //makes the measure length by dividing seconds per minute by measures per minute
-        let mesLen = 60.0 / tempo
+        let mesLen = 60.0 / this.Tempo
         let mutable output = null
         for i = 0 to input.Length - 1 do
             let dur = 
